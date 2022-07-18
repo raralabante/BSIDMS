9d065d61a9a0202594614a3bc5cd68cbaa40393b
kaltstein <109489007+kaltstein@users.noreply.github.com>
2022-07-18 09:36:55 +0800

    Undo moving of index and htaccess

--- public/index.php
+++ public/index.php
@@ -30,8 +30,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 | into the script here so we don't need to manually load our classes.
 |
 */
+require __DIR__.'/../vendor/autoload.php';
 
-require __DIR__.'/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__.'/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
