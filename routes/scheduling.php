f8e445f7bd32d2b1e03d1bd568a236d409dd4c09
kaltstein <109489007+kaltstein@users.noreply.github.com>
2022-08-03 15:48:40 +0800

    Update scheduling.php

--- routes/scheduling.php
+++ routes/scheduling.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/schedulingmaster', [App\Http\Controllers\SchedulingMasterController::class, 'index'])
 ->middleware('role:Administrator,Scheduling Manager,Scheduling Admin,Senior Scheduler')->name('scheduling_master');
+Route::get('/schedulingmaster/list', [App\Http\Controllers\SchedulingMasterController::class, 'schedulingMasterList'])->name('scheduling_master.list');
+Route::post('/schedulingmaster/insert', [App\Http\Controllers\SchedulingMasterController::class, 'insert'])->name('scheduling_master.insert');
+Route::get('/schedulingmaster/fetch/{id}', [App\Http\Controllers\SchedulingMasterController::class, 'fetch'])->name('scheduling_master.fetch');
